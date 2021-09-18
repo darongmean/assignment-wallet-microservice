5f1f19f5d68b554c9cc133f79a417c449f5aabb1
Darong Mean <contact.darong@gmail.com>
2021-09-18 16:52:57 +0700

    Refactor: Extract generator for future use

--- src/test/java/com/darongmean/integration_test/H2DBTest.java
+++ src/test/java/com/darongmean/integration_test/H2DBTest.java
@@ -2,18 +2,15 @@ package com.darongmean.integration_test;
 
 import com.darongmean.h2db.TBalanceTransaction;
 import com.darongmean.h2db.TBalanceTransactionRepository;
+import com.darongmean.testutil.PropertyBasedTest;
 import io.quarkus.test.TestTransaction;
 import io.quarkus.test.junit.QuarkusTest;
-import net.jqwik.api.Arbitraries;
-import net.jqwik.time.api.DateTimes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.RepeatedTest;
 
 import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
-import java.math.BigDecimal;
-import java.time.ZoneId;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,13 +26,13 @@ public class H2DBTest {
     @TestTransaction
     void testNotThrowExceptionWhenPersistValidData() {
         TBalanceTransaction tBalanceTransaction = new TBalanceTransaction();
-        genPlayerId(tBalanceTransaction);
-        genTotalBalance(tBalanceTransaction);
-        genTransactionAmount(tBalanceTransaction);
-        genTransactionType(tBalanceTransaction);
-        getTransactionId(tBalanceTransaction);
-        genCreatedAt(tBalanceTransaction);
-        getTraceId(tBalanceTransaction);
+        tBalanceTransaction.setPlayerId(PropertyBasedTest.genPlayerId().sample());
+        tBalanceTransaction.setTotalBalance(PropertyBasedTest.genTotalBalance().sample());
+        tBalanceTransaction.setTransactionAmount(PropertyBasedTest.genTransactionAmount().sample());
+        tBalanceTransaction.setTransactionType(PropertyBasedTest.genTransactionType().sample());
+        tBalanceTransaction.setTransactionId(PropertyBasedTest.genTransactionId().sample());
+        tBalanceTransaction.setCreatedAt(PropertyBasedTest.genCreatedAt().sample());
+        tBalanceTransaction.setTraceId(PropertyBasedTest.genTraceId().sample());
 
         assertGenerateValidData(tBalanceTransaction);
 
@@ -43,42 +40,6 @@ public class H2DBTest {
 
         Assertions.assertTrue(tBalanceTransactionRepository.isPersistent(tBalanceTransaction));
         Assertions.assertTrue(tBalanceTransaction.getBalanceTransactionPk() > 0);
-    }
-
-    private void getTraceId(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setTraceId(Arbitraries.strings().injectNull(0.1).sample());
-    }
-
-    private void genCreatedAt(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setCreatedAt(DateTimes.dateTimes().sample().atZone(ZoneId.systemDefault()));
-    }
-
-    private void getTransactionId(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setTransactionId(genNonEmptyString());
-    }
-
-    private void genTransactionAmount(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setTransactionAmount(genMoneyAmount());
-    }
-
-    private BigDecimal genMoneyAmount() {
-        return Arbitraries.bigDecimals().ofScale(4).between(BigDecimal.ZERO, new BigDecimal("999999999.9999")).sample();
-    }
-
-    private void genTransactionType(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setTransactionType(Arbitraries.of("debit", "credit").sample());
-    }
-
-    private void genPlayerId(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setPlayerId(genNonEmptyString());
-    }
-
-    private String genNonEmptyString() {
-        return Arbitraries.strings().ofMinLength(1).ofMaxLength(255).filter(v -> v.trim().length() > 0).sample();
-    }
-
-    private void genTotalBalance(TBalanceTransaction tBalanceTransaction) {
-        tBalanceTransaction.setTotalBalance(genMoneyAmount());
     }
 
     private void assertGenerateValidData(TBalanceTransaction tBalanceTransaction) {
---
+++ src/test/java/com/darongmean/testutil/PropertyBasedTest.java
@@ -0,0 +1,57 @@
+package com.darongmean.testutil;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.Provide;
+import net.jqwik.api.arbitraries.BigDecimalArbitrary;
+import net.jqwik.time.api.DateTimes;
+
+import java.math.BigDecimal;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class PropertyBasedTest {
+
+    @Provide
+    public static Arbitrary<String> genTraceId() {
+        return Arbitraries.strings().injectNull(0.1);
+    }
+
+    @Provide
+    public static Arbitrary<ZonedDateTime> genCreatedAt() {
+        return DateTimes.dateTimes().map(d -> d.atZone(ZoneId.systemDefault()));
+    }
+
+    @Provide
+    public static Arbitrary<String> genTransactionId() {
+        return genNonEmptyString();
+    }
+
+    @Provide
+    public static BigDecimalArbitrary genTransactionAmount() {
+        return genMoneyAmount();
+    }
+
+    private static BigDecimalArbitrary genMoneyAmount() {
+        return Arbitraries.bigDecimals().ofScale(4).between(BigDecimal.ZERO, new BigDecimal("999999999.9999"));
+    }
+
+    @Provide
+    public static Arbitrary<String> genTransactionType() {
+        return Arbitraries.of("debit", "credit");
+    }
+
+    @Provide
+    public static Arbitrary<String> genPlayerId() {
+        return genNonEmptyString();
+    }
+
+    private static Arbitrary<String> genNonEmptyString() {
+        return Arbitraries.strings().ofMinLength(1).ofMaxLength(255).filter(v -> v.trim().length() > 0);
+    }
+
+    @Provide
+    public static BigDecimalArbitrary genTotalBalance() {
+        return genMoneyAmount();
+    }
+}
