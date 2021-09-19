61075a14df01031459b19798439e9b7f0c4799e1
Darong Mean <contact.darong@gmail.com>
2021-09-19 23:27:23 +0700

    Refactor: Split statement for better management

---
+++ src/main/resources/db/migration/V20210919000000__drop-constraint-tbalancetransaction.sql
@@ -0,0 +1,2 @@
+alter table TBalanceTransaction
+    drop constraint uq_transactionId_on_TBalanceTransaction;
--- src/main/resources/db/migration/V20210919000010__create-index-transactionId.sql
+++ src/main/resources/db/migration/V20210919000010__create-index-transactionId.sql
@@ -1,5 +1,2 @@
-alter table TBalanceTransaction
-    drop constraint uq_transactionId_on_TBalanceTransaction;
-
 create index ix_transactionId_on_TBalanceTransaction
     on TBalanceTransaction(transactionId);
