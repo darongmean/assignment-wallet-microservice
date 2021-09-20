238f85da383000fcdd9c9144710ec1b4234b7b0e
Darong Mean <contact.darong@gmail.com>
2021-09-20 10:51:21 +0700

    Doc: Update swagger document

--- src/main/java/com/darongmean/http/RouteV1.java
+++ src/main/java/com/darongmean/http/RouteV1.java
@@ -1,17 +1,25 @@
 package com.darongmean.http;
 
 import com.darongmean.balance.BalanceRequest;
+import com.darongmean.balance.BalanceResponse;
 import com.darongmean.balance.GetBalance;
 import com.darongmean.common.ErrorResponse;
 import com.darongmean.credit.CreditRequest;
+import com.darongmean.credit.CreditResponse;
 import com.darongmean.credit.IncreaseBalance;
 import com.darongmean.debit.DebitRequest;
+import com.darongmean.debit.DebitResponse;
 import com.darongmean.debit.DecreaseBalance;
 import com.darongmean.h2db.TBalanceTransactionRepository;
 import com.darongmean.transaction.GetHistory;
 import com.darongmean.transaction.HistoryRequest;
 import com.darongmean.transaction.HistoryResponse;
 import io.opentracing.Tracer;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
@@ -33,6 +41,16 @@ public class RouteV1 {
 
     @GET
     @Path("/balance")
+    @Operation(summary = "Get current balance of a player")
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Current balance of the player",
+                    content = @Content(schema = @Schema(implementation = BalanceResponse.class))),
+            @APIResponse(
+                    responseCode = "404",
+                    description = "No balance transaction found",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     public Response getBalance(@BeanParam BalanceRequest balanceRequest) {
         GetBalance getBalance = new GetBalance(tBalanceTransactionRepository);
         getBalance.execute(balanceRequest);
@@ -48,6 +66,8 @@ public class RouteV1 {
 
     @GET
     @Path("/transaction")
+    @Operation(summary = "Get transaction history of a player")
+    @APIResponse(responseCode = "200", description = "Transaction history of the player")
     public HistoryResponse getTransaction(@BeanParam HistoryRequest historyRequest) {
         GetHistory getHistory = new GetHistory(tBalanceTransactionRepository);
         getHistory.execute(historyRequest);
@@ -57,6 +77,18 @@ public class RouteV1 {
     @POST
     @Path("/credit")
     @Transactional
+    @Operation(
+            summary = "Submit credit transaction of a player",
+            description = "Adding fund to the balance of a player")
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Current balance of the player",
+                    content = @Content(schema = @Schema(implementation = CreditResponse.class))),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "The transaction rejected",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     public Response postCredit(CreditRequest creditRequest) {
         creditRequest.setTraceId(getTraceId());
 
@@ -75,6 +107,18 @@ public class RouteV1 {
     @POST
     @Path("/debit")
     @Transactional
+    @Operation(
+            summary = "Submit debit transaction of a player",
+            description = "Removing fund from the balance of a player")
+    @APIResponses(value = {
+            @APIResponse(
+                    responseCode = "200",
+                    description = "Current balance of the player",
+                    content = @Content(schema = @Schema(implementation = DebitResponse.class))),
+            @APIResponse(
+                    responseCode = "400",
+                    description = "The transaction rejected",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     public Response postDedit(DebitRequest debitRequest) {
         debitRequest.setTraceId(getTraceId());
 
